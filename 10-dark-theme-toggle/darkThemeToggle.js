bdc7c183cc91422f9301819264bb8e7cc0e805e8
rayanahmed <thismac@Shrabonis-Air.Dlink>
2025-01-26 23:03:00 +0600

    raw css dark mode toggle is done now

--- 10-dark-theme-toggle/darkThemeToggle.js
+++ 10-dark-theme-toggle/darkThemeToggle.js
@@ -1,18 +1,32 @@
 const htmlElement = document.documentElement;
 const themeToggleButton = document.getElementById("theme-toggle");
 
+// themeToggleButton.addEventListener("click", () => {
+//   htmlElement.classList.toggle("dark");
+//   localStorage.setItem(
+//     "theme",
+//     htmlElement.classList.contains("dark") ? "dark" : "light"
+//   );
+
+// });
+
+// const savedTheme = localStorage.getItem("theme");
+// if (savedTheme) {
+//   htmlElement.classList.add(savedTheme);
+// } else {
+//   htmlElement.classList.add("dark");
+// }
+
 themeToggleButton.addEventListener("click", () => {
   htmlElement.classList.toggle("dark");
   localStorage.setItem(
     "theme",
     htmlElement.classList.contains("dark") ? "dark" : "light"
   );
-
 });
 
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme) {
-  htmlElement.classList.add(savedTheme);
-} else {
+if (localStorage.getItem("theme") === "dark") {
   htmlElement.classList.add("dark");
+} else {
+  htmlElement.classList.remove("dark");
 }
