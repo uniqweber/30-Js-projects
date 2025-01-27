d939d71b2918a7f1a7a9110fe6fdfbacab123a16
rayanahmed <thismac@Shrabonis-Air.Dlink>
2025-01-27 21:02:26 +0600

    the coming soon page with email send too google sheet is done

--- 13-email-to-sheet/emailToSheet.js
+++ 13-email-to-sheet/emailToSheet.js
@@ -1,1 +1,29 @@
-console.log('working');
+const URL =
+  "https://script.google.com/macros/s/AKfycbxHODbsmCdWe--ovUWe2F6YgnPrQsbnIQOTyAvKW1q0fsL0-iP3nu9qvThCiMOcNxo/exec";
+
+const form = document.querySelector("form");
+const input = document.getElementById("email");
+const button = document.querySelector("button");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = input.value.trim();
+  if (email) {
+    button.disabled = true;
+    button.innerHTML = "Sending...";
+    fetch(URL, {
+      method: "POST",
+      body: new FormData(form),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          form.reset();
+          button.disabled = false;
+          button.innerHTML = "Notify Me";
+        }
+        alert("Thank you for subscribing!");
+      })
+      .catch((err) => console.error(err));
+  }
+});
