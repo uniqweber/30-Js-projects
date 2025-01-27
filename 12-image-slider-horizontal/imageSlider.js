45b43ea729f9763aa76f17d3a15c847cdc624cc8
rayanahmed <thismac@Shrabonis-Air.Dlink>
2025-01-27 20:02:48 +0600

    auto slide functionality add

--- 12-image-slider-horizontal/imageSlider.js
+++ 12-image-slider-horizontal/imageSlider.js
@@ -1,7 +1,34 @@
 const slider = document.getElementById("slider");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
+const leftButton = document.getElementById("leftButton");
+const rightButton = document.getElementById("rightButton");
 
+const slides = slider.children.length - 1;
+let currentSlide = 0;
+let autoSlideTime = 3000;
 
+function moveSlider(index) {
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}
 
+function nextSlide() {
+  currentSlide = currentSlide < slides ? ++currentSlide : 0;
+  moveSlider(currentSlide);
+}
 
+function prevSlide() {
+  currentSlide = currentSlide > 0 ? --currentSlide : slides - 1;
+  moveSlider(currentSlide);
+}
+
+let autoSlide = setInterval(nextSlide, autoSlideTime);
+
+rightButton.addEventListener("click", () => {
+  clearInterval(autoSlide);
+  nextSlide();
+  autoSlide = setInterval(nextSlide, autoSlideTime);
+});
+leftButton.addEventListener("click", () => {
+  clearInterval(autoSlide);
+  prevSlide();
+  autoSlide = setInterval(nextSlide, autoSlideTime);
+});
