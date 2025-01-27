24bf1d62a9b26964722f906e69a118cca1f26248
rayanahmed <thismac@Shrabonis-Air.Dlink>
2025-01-27 23:22:36 +0600

    the password strength checker now is done

--- 14-password-strength/passwordStrength.js
+++ 14-password-strength/passwordStrength.js
@@ -4,7 +4,9 @@ const confirmPassword = document.getElementById("confirmPassword");
 const strengthMsg = document.getElementById("strengthMsg");
 const matchMsg = document.getElementById("matchMsg");
 
-let setPassword = false;
+let setPassword = "";
+let matchPassword = "";
+
 password.addEventListener("input", () => {
   const upperCase = /[A-Z]/;
   const lowerCase = /[a-z]/;
@@ -17,39 +19,39 @@ password.addEventListener("input", () => {
 
   if (passwordValue.length === 0) {
     strengthMsg.classList.add("hidden");
-    setPassword = false;
+    setPassword = "";
   } else if (passwordValue.length < minLength) {
     strengthMsg.textContent = "Password must be at least 8 characters";
     strengthMsg.classList.remove("hidden", "text-green-600");
     strengthMsg.classList.add("text-red-500");
-    setPassword = false;
+    setPassword = "";
   } else if (!upperCase.test(passwordValue)) {
     strengthMsg.textContent =
       "Password must contain at least one uppercase letter";
     strengthMsg.classList.remove("hidden");
     strengthMsg.classList.add("text-red-500");
-    setPassword = false;
+    setPassword = "";
   } else if (!lowerCase.test(passwordValue)) {
     strengthMsg.textContent =
       "Password must contain at least one lowercase letter";
     strengthMsg.classList.remove("hidden");
     strengthMsg.classList.add("text-red-500");
-    setPassword = false;
+    setPassword = "";
   } else if (!number.test(passwordValue)) {
     strengthMsg.textContent = "Password must contain at least one number";
     strengthMsg.classList.remove("hidden");
     strengthMsg.classList.add("text-red-500");
-    setPassword = false;
+    setPassword = "";
   } else if (!specialChar.test(passwordValue)) {
     strengthMsg.textContent =
       "Password must contain at least one special character";
     strengthMsg.classList.remove("hidden");
     strengthMsg.classList.add("text-red-500");
-    setPassword = false;
+    setPassword = "";
   } else {
     strengthMsg.textContent = "Password is strong";
     strengthMsg.classList.add("text-green-600");
-    setPassword = true;
+    setPassword = passwordValue;
   }
 });
 
@@ -59,7 +61,7 @@ confirmPassword.addEventListener("input", () => {
   if (confirmLength === 0) {
     matchMsg.classList.add("hidden");
     matchMsg.classList.remove("text-red-500", "text-green-600");
-  } else if (setPassword) {
+  } else if (setPassword !== "") {
     matchMsg.classList.remove("hidden");
     if (password.value !== confirmPassword.value) {
       matchMsg.classList.add("text-red-500");
@@ -67,6 +69,7 @@ confirmPassword.addEventListener("input", () => {
     } else {
       matchMsg.classList.add("text-green-600");
       matchMsg.textContent = "Password matches";
+      matchPassword = confirmPassword.value;
     }
   } else {
     matchMsg.classList.remove("hidden", "text-green-600");
@@ -77,13 +80,15 @@ confirmPassword.addEventListener("input", () => {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (setPassword && password.value === confirmPassword.value) {
+  if (setPassword && matchPassword) {
     strengthMsg.classList.add("hidden");
     strengthMsg.classList.remove("text-red-500");
     matchMsg.classList.add("hidden");
     matchMsg.classList.remove("text-red-500");
-    console.log("Form submitted", password.value, confirmPassword.value);
+    console.log("Form submitted", setPassword, matchPassword);
     form.reset();
+    setPassword = "";
+    matchPassword = "";
   } else {
     strengthMsg.classList.remove("hidden");
     strengthMsg.classList.add("text-red-500");
